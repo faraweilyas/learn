79eca588d35a19c1b4c1e64f27a24e975288bba2
iLyas Farawe <faraweilyas@gmail.com>
2020-05-05 22:25:59 +0100

    Adds error log

--- slim-api/src/Chatter/Middleware/Logging.php
+++ slim-api/src/Chatter/Middleware/Logging.php
@@ -6,7 +6,7 @@ class Logging
 {
     public function __invoke($request, $response, $next)
     {
-        // error_log($request->getMethod() . " -- " . $request->getUri());
+        error_log($request->getMethod() . " -- " . $request->getUri());
         return $next($request, $response);
     }
 }
