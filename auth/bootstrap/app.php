3b64dd3948c85468339e25f7775420d0c617736d
iLyas Farawe <faraweilyas@gmail.com>
2020-05-23 02:22:59 +0100

    - Clean up code - Adds router package

--- auth/bootstrap/app.php
+++ auth/bootstrap/app.php
@@ -5,12 +5,14 @@ session_start();
 require_once __DIR__."/../vendor/autoload.php";
 
 try {
-	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..//');
-	$dotenv->load();
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..//')->load();
 } catch (Dotenv\Exception\InvalidPathException $exception)
 {
 	// 
 }
 
-var_dump(getenv('APP_ENV'));
-var_dump(getenv('APP_NAME'));
+var_dump(
+	getenv('APP_NAME')
+	, $_ENV['APP_NAME']
+	, $_SERVER['APP_NAME']
+);
