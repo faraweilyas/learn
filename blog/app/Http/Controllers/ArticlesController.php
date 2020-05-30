4ca303f7ba580aab9a9efe378ccd65ea1ceef3ff
iLyas Farawe <faraweilyas@gmail.com>
2020-05-30 02:33:01 +0100

    Adds controllers

---
+++ blog/app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::latest()->get();
+
+        return view('articles.index', compact('articles'));
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', compact('article'));
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        Article::create($this->validateArticle());
+
+        return redirect(route('articles.index'));
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update(Article $article)
+    {
+        $article->update($this->validateArticle());
+
+        return redirect($article->path());
+    }
+
+    public function destroy()
+    {
+        //
+    }
+
+    public function validateArticle()
+    {
+        return request()->validate([
+            'title'     => 'required',
+            'excerpt'   => 'required',
+            'body'      => 'required',
+        ]);
+    }
+}
