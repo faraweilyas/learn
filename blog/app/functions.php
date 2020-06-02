c19bf067a087d402548fb318dd8dd818a4f04afd
iLyas Farawe <faraweilyas@gmail.com>
2020-06-03 00:32:14 +0100

    Adds more functions

--- blog/app/functions.php
+++ blog/app/functions.php
@@ -41,3 +41,33 @@ function activateLink(string $path) : string
 {
     return (Request::path() == $path) ? " class='current_page_item'" : "";
 }
+
+/**
+ * Removes some part of a given string starting from the end
+ *
+ * @param string $value
+ * @param int $length
+ * @return string
+ */
+function stripper(string $value, int $length=1) : string
+{
+    $valueLength = strlen(trim($value));
+    return substr($value, 0, $valueLength - $length);
+}
+
+/**
+ * Formats a given amount
+ *
+ * @param string $digit
+ * @param bool $round
+ * @param mixed $prefix
+ * @return string
+ */
+function formatAmount(string $digit, bool $round=FALSE, $prefix="&#8358;") : string
+{
+    if (empty($digit)) return "";
+    if (!$round)
+        return $prefix.number_format($digit, 2, '.', ',');
+    else
+        return stripper($prefix.number_format($digit, 2, '.', ','), 3);
+}
