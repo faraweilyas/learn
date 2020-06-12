a3689bdb8a5b46174f2606853eacbb7c5c44ab46
iLyas Farawe <faraweilyas@gmail.com>
2020-06-12 20:27:10 +0100

    defines a gate logic

--- blog/app/Providers/AuthServiceProvider.php
+++ blog/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\User;
+use App\Conversation;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-coversation', function(User $user, Conversation $conversation)
+        {
+            return $conversation->user->is($user);
+        });
     }
 }
