0e835adb6f875fed55ecb102db55d87e909a1e2c
iLyas Farawe <faraweilyas@gmail.com>
2020-06-13 22:00:01 +0100

    Adds tweet views

---
+++ tweety/resources/views/_publish_tweet_panel.blade.php
@@ -0,0 +1,21 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="" method="">
+        <textarea
+            name="body"
+            class="w-full"
+            placeholder="What's up doc?"
+        ></textarea>
+
+        <hr class="my-4" />
+
+        <footer class="flex justify-between">
+            <img
+                class="rounded-full mr-2"
+                src="https://i.pravatar.cc/40"
+                alt=""
+            />
+
+            <button type="submit" class="bg-blue-400 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+        </footer>
+    </form>
+</div>
---
+++ tweety/resources/views/_tweet.blade.php
@@ -0,0 +1,15 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            class="rounded-full mr-2"
+            src="https://i.pravatar.cc/50"
+            alt=""
+        />
+
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah</p>
+    </div>
+</div>
