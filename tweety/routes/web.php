5f0e6f808a7b0f56712f2eb57205fc061bd30d78
iLyas Farawe <faraweilyas@gmail.com>
2020-06-14 23:07:36 +0100

    - Refactored code - Adds middleware authorization

--- tweety/routes/web.php
+++ tweety/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// auth()->loginUsingId(2);
+// auth()->loginUsingId(3);
 
 // DB::listen(function($query)
 // {
@@ -16,17 +16,25 @@ Route::get('/', function()
 
 Route::middleware('auth')->group(function()
 {
-    Route::get('/tweets', 'TweetController@index')->name('tweety.home');
+    Route::get('/tweets', 'TweetController@index')
+        ->name('tweety.home');
 
-    Route::post('/tweets', 'TweetController@store')->name('tweety.tweet');
+    Route::post('/tweets', 'TweetController@store')
+        ->name('tweety.tweet');
 
-    Route::post('/profiles/{user:username}/follow', 'FollowController@store')->name('tweety.user_follow');
+    Route::post('/profiles/{user:username}/follow', 'FollowController@store')
+        ->name('tweety.user_follow');
 
-    Route::get('/profiles/{user:username}/edit', 'ProfileController@edit')->name('tweety.user_edit')->middleware('can:edit,user');
+    Route::get('/profiles/{user:username}/edit', 'ProfileController@edit')
+        ->name('tweety.user_edit')
+        ->middleware('can:edit,user');
 
-    Route::patch('/profiles/{user:username}', 'ProfileController@update')->name('tweety.user_update');
+    Route::patch('/profiles/{user:username}', 'ProfileController@update')
+        ->name('tweety.user_update')
+        ->middleware('can:edit,user');
 });
 
-Route::get('/profiles/{user:username}', 'ProfileController@show')->name('tweety.user_profile');
+Route::get('/profiles/{user:username}', 'ProfileController@show')
+    ->name('tweety.user_profile');
 
 Auth::routes();
