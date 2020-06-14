ab57d6871feb97f967723dc55d7827e8680bd2c2
iLyas Farawe <faraweilyas@gmail.com>
2020-06-14 22:29:57 +0100

    Adds new routes

--- tweety/routes/web.php
+++ tweety/routes/web.php
@@ -2,22 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// auth()->loginUsingId(2);
+
+// DB::listen(function($query)
+// {
+//     dump($query->sql, $query->bindings);
+// });
 
 Route::get('/', function()
 {
     return view('welcome');
 });
 
-Auth::routes();
+Route::middleware('auth')->group(function()
+{
+    Route::get('/tweets', 'TweetController@index')->name('tweety.home');
 
-Route::get('/home', 'HomeController@index')->name('home');
+    Route::post('/tweets', 'TweetController@store')->name('tweety.tweet');
+
+    Route::post('/profiles/{user:username}/follow', 'FollowController@store')->name('tweety.user_follow');
+
+    Route::get('/profiles/{user:username}/edit', 'ProfileController@edit')->name('tweety.user_edit')->middleware('can:edit,user');
+
+    Route::patch('/profiles/{user:username}', 'ProfileController@update')->name('tweety.user_update');
+});
+
+Route::get('/profiles/{user:username}', 'ProfileController@show')->name('tweety.user_profile');
+
+Auth::routes();
