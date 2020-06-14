f7ed1e73928602b5ebe279d54ed313ff34c48a42
iLyas Farawe <faraweilyas@gmail.com>
2020-06-14 23:12:43 +0100

    Refactors web routes

--- tweety/routes/web.php
+++ tweety/routes/web.php
@@ -16,25 +16,43 @@ Route::get('/', function()
 
 Route::middleware('auth')->group(function()
 {
-    Route::get('/tweets', 'TweetController@index')
-        ->name('tweety.home');
+    Route::get(
+        '/tweets',
+        'TweetController@index'
+    )
+    ->name('tweety.home');
 
-    Route::post('/tweets', 'TweetController@store')
-        ->name('tweety.tweet');
+    Route::post(
+        '/tweets',
+        'TweetController@store'
+    )
+    ->name('tweety.tweet');
 
-    Route::post('/profiles/{user:username}/follow', 'FollowController@store')
-        ->name('tweety.user_follow');
+    Route::post(
+        '/profiles/{user:username}/follow',
+        'FollowController@store'
+    )
+    ->name('tweety.user_follow');
 
-    Route::get('/profiles/{user:username}/edit', 'ProfileController@edit')
-        ->name('tweety.user_edit')
-        ->middleware('can:edit,user');
+    Route::get(
+        '/profiles/{user:username}/edit',
+        'ProfileController@edit'
+    )
+    ->name('tweety.user_edit')
+    ->middleware('can:edit,user');
 
-    Route::patch('/profiles/{user:username}', 'ProfileController@update')
-        ->name('tweety.user_update')
-        ->middleware('can:edit,user');
+    Route::patch(
+        '/profiles/{user:username}',
+        'ProfileController@update'
+    )
+    ->name('tweety.user_update')
+    ->middleware('can:edit,user');
 });
 
-Route::get('/profiles/{user:username}', 'ProfileController@show')
-    ->name('tweety.user_profile');
+Route::get(
+    '/profiles/{user:username}',
+    'ProfileController@show'
+)
+->name('tweety.user_profile');
 
 Auth::routes();
