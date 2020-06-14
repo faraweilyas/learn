a39dea5ff1693129e142932f9ac955f213164b16
iLyas Farawe <faraweilyas@gmail.com>
2020-06-14 22:25:25 +0100

    - Adds followable trait - Adds new methods

--- tweety/app/User.php
+++ tweety/app/User.php
@@ -8,32 +8,54 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Followable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'avatar', 'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getAvatarAttribute($avatar)
+    {
+        return !empty($avatar)
+            ? asset('storage/'.$avatar)
+            : "https://i.pravatar.cc/200?u={$this->email}";
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function timeline()
+    {
+        $ids = $this->follows()->pluck('id');
+
+        return Tweet::whereIn('user_id', $ids)
+            ->orWhere('user_id', $this->id)
+            ->latest()
+            ->get();
+
+        // $ids->push($this->id);
+        // return Tweet::whereIn('user_id', $ids)->latest()->get();
+    }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class)->latest();
+    }
+
+    public function user_profile()
+    {
+        return (Route::currentRouteName() == "tweety.user_profile")
+            ? auth()->user()
+            : auth()->user();
+    }
 }
