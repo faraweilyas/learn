4c4f7e277cedd010abac0204e048db7b90fc24a2
iLyas Farawe <faraweilyas@gmail.com>
2020-06-16 23:25:24 +0100

    - Adds hover style - Adds register link

--- tweety/resources/views/_sidebar_links.blade.php
+++ tweety/resources/views/_sidebar_links.blade.php
@@ -1,28 +1,28 @@
 <ul>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="{{ route('tweety.home') }}">Home</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="{{ route('tweety.home') }}">Home</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="{{ route('tweety.user_profile', auth()->user()) }}">Pofile</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="{{ route('tweety.user_profile', auth()->user()) }}">Pofile</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/explore">Explore</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="/explore">Explore</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="#">Notifications</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="#">Notifications</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="#">Messages</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="#">Messages</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="#">Bookmarks</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="#">Bookmarks</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="#">Lists</a>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="#">Lists</a>
     </li>
     <li>
         <a
-            class="font-bold text-lg mb-4 block"
+            class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block"
             href="{{ route('logout') }}"
             onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
         >
@@ -31,5 +31,8 @@
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
+    </li>
+    <li>
+        <a class="font-bold hover:underline hover:text-blue-500 text-lg mb-4 block" href="{{ route('register') }}">Register</a>
     </li>
 </ul>
